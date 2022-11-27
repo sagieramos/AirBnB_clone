e0beb0a2c7a25600f5c457cbd2a5e88a5723cea6
calvean <garnetsharara@gmail.com>
2022-11-27 20:25:08 +0200

    removed cache

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import cmd
 import sys
 import json
 import os
-from models import storage
+from models._init_ import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
