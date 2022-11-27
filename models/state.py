11040500588c7bbf3fac7bb25a712b0dd5749892
calvean <garnetsharara@gmail.com>
2022-11-27 18:43:51 +0200

    state class

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represents state.
+    Attributes:
+        name (str): The name of state.
+    """
+
+    name = ""
